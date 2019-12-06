7560a21fdb365a37095ecb039288bf022f77e38c
Justin Bermudez <justinanbermudez@gmail.com>
2019-12-06 11:24:51 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  return string.toUpperCase();
+}
+
+function logWhisper(string) {
+  return string.toLowerCase();
+}
+
+function sayHiToGrandma(string){
+  if(string.toLowerCase() === lowercase) {
+    return("I can't hear you!");
+  }
+  if(string.toUpperCase() === uppercase) {
+    return("YES INDEED!");
+  }
+  if(string === "I love you, Grandma.") {
+    return("I love you, too.");
+  }
+}
